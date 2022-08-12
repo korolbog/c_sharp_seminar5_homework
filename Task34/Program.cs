8c289a2a186e770da139baee0a68d326dee83b22
BogdanKorolev <bkorolevjob@gmail.com>
2022-08-12 18:27:43 +0300

    Task34 code written

---
+++ Task34/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 34:
+Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+Task34();
+void Task34()
+{
+    int size = new Random().Next(4,6);
+    int[] array = new int[size];
+    int count = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Чётных чисел в массиве: {count}");
+}
